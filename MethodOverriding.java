70252a2ee4222d9c6b7e78b1ea66b70f5110b734
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-11 12:54:03 +0300

    Add comments explaining method overriding concept
    Added explanation of method overriding in Java.

---
+++ MethodOverriding.java
@@ -0,0 +1,7 @@
+Method Overriding : when you write a method in SubClass that already has in SuperClass but there is a different that is the task in the method
+but you should inheritance it and same name of method 
+
+that is not necessarily but good if you do it :
+write @methodoverriding before the method that you wanna overriding it
+
+  
