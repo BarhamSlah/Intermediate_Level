b967ef9a686c35669c61533a0e4bd014f2c454fd
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-11 15:24:05 +0300

    Update MethodOverriding.java

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -1,7 +1,21 @@
-Method Overriding : when you write a method in SubClass that already has in SuperClass but there is a different that is the task in the method
+Method Override: when you write a method in SubClass that already has in SuperClass but there is a different that is the task in the method
 but you should inheritance it and same name of method 
 
 that is not necessarily but good if you do it :
 write @methodoverriding before the method that you wanna overriding it
+
+****************************************************************************************************************************  
+//   THIS METHOD IS IN THE SUPERCLASS
+ void start(){
+    System.out.println("this car is going");
+  } 
+
+// THIS METHOD IS IN THE SUBCLASS
+@Override
+void start(){
+    System.out.println("this car is flying");
+ }
+
+***************************************************************************************************************************
 
   
