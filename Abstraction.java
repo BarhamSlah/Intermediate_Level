f5f6d9e26c642017b169d0dc5655f8f406a624a9
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-13 22:52:44 +0300

    Update Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -1,1 +1,10 @@
+Abstraction : is use to hide detail and display just the important things
+1 - You write something in SuperClass (what to do )
+2 - You write something in SubClass (how to do)
 
+Abstraction has 2 types : 1- Abstract Class  &  2- Interface  
+
+Abstract Class: It can contain both regular methods (with code/body) and abstract methods (without code/body).
+  
+Interface: It is completely abstract: it contains only and exclusively abstract methods
+(without code/body) which the subclasses must write entirely by themselves.  
