11a72c2dc7c0c24b54b1c6caeb3ba473ff5e21b7
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-19 14:38:08 +0300

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -6,6 +6,14 @@ Interface :
   
 2-  Subclass can have multiple Interface but not Superclass
 
-3-  When you declare variable in interface you can't change value of it   
+3-  When you declare variable in interface you can't change value of it 
+  
+*************************************************************************************
+Class to Class: Use extends (A class inherits from another class).
+  
+Class to Interface: Use implements (A class fulfills the contract of an interface).
+  
+Interface to Interface: Use extends (An interface can expand upon another interface).
+**************************************************************************************
 
   
