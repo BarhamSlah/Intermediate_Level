cf6ae25b294017e9acbadddc5f58ffed248327cf
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-19 14:24:18 +0300

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,1 +1,11 @@
+Interface very similiar to abstract class 
+  
+Interface : 
+  
+1-  In Interface you can write what to do not how to do 
+  
+2-  Subclass can have multiple Interface but not Superclass
 
+3-  When you declare variable in interface you can't change value of it   
+
+  
