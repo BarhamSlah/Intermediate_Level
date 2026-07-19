f7931d12054ea64b7a3b1467b69b681d70a73bef
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-19 16:34:56 +0300

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,1 +1,9 @@
+// WE HAVE 2 TYPES OF POLYPORPHISM
 
+1- STATIC POLYMORPHISM (COMPILE TIME) (METHOD OVERLOADING)
+
+2- DYNAMIC POLYMORPHISM (RUNTIME) (METHOD OVERRIDING)
+***************************************************************************************************
+
+  
+  
