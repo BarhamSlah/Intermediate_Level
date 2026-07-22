34981b7bb3fe9bbbee65146c7f33c51257ed460a
BarhamSlah <barhamsalahaddin@gmail.com>
2026-07-22 21:56:08 +0300

    Update Getter&Setter.java

--- Getter&Setter.java
+++ Getter&Setter.java
@@ -1,1 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Car car = new Car(18_300,2012, "Toyota-Hiluix");
+
+
+        /*
+        car.model = 2026;  now the model is 2026 not 2013
+        because the variable is public not private
+        but when public int model = 2013; you can't change unless in the object
+
+
+        the public variable won't work just like private variable
+        you should write setter or getter or  both of them
+        */
+
+
+        // THIS IS FOR PUBLIC VARIABLE
+      //  System.out.println(car.name + " model of car " + car.model + " by this price " + car.price);
+
+//WHICH SETTER OR GETTER CREATE AT LAST THE OUTPUT IS ITS
+// SETTER AND GETTER HAS SAME OUTPUT BUT IN DIFFERENT WAY
+car.setName("Ferrari");
+car.setPrice(440_000);
+car.setModel(2025);
+
+       System.out.println(car.getName() + " model of car " + car.getModel() + " by this price " + car.getPrice());
+
+    }
+    }
+/////////////////////////////////////////////////
+public class Car {
+
+   private String name;
+   private int price;
+   private int model;
+
+    Car(int price , int model , String name ){
+        this.model = model;
+        this.price = price;
+        this.name = name;
+    }
+// THIS IS ALSO GETTER & THE DATATYPE IS SELECT BASED ON WHAT IS THE DATATYPE OF RETURN
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPrice(){
+        return "$" + this.price;
+    }
+
+    public int getModel(){
+        return this.model;
+    }
+
+    //////////////
+
+    // THIS IS ALSO SETTER YOU MAKE PARAMETER & ARGUMENT
+
+    public void setName(String name){
+this.name = name;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public void setModel(int model){
+        this.model = model;
+    }
+}
+***************************************************************************************************************************
