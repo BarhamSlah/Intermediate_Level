b6bbd185b74d85b3bd67978db6c93154ef16258a
Barham Slah <barhamsalahaddin@gmail.com>
2026-08-03 18:14:14 +0300

    Add info about Exception-Handling file

---
+++ Exception-Handling.java
@@ -0,0 +1,14 @@
+/*
+      finally always execute
+      when you just write Exception you don't select the error so the user doesn't know what is the problem
+      So always select specific exception
+      put things inside try
+      you should write letter or word just after Exception in catch
+      Never leave catch block empty
+
+
+      Golden Rule: If you can prevent an error with a simple `if` statement, use `if`.
+        But if the error is related to an external system (like a database or a file) that you don't have control over,
+        then use `try-catch`.
+*/
+
