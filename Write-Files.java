a619d445549f0d80e8484b7c858d12869024e3d1
Barham Slah <barhamsalahaddin@gmail.com>
2026-08-04 02:03:28 +0300

    Add Write-Files & The 4 Classes

---
+++ Write-Files.java
@@ -0,0 +1,8 @@
+All of them are Java classes used to write text to files
+
+Classes        :   When to use it?
+FileWriter :  For small text files.
+BufferedWriter :  For large amounts of text and big files
+PrintWriter :  For creating structured logs and reports.
+FileOutputStream :  For binary files (images, audio, video).
+
