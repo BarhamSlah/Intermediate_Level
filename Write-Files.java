e1c6a8f9464e4088de4785c06fb60bf6d46d2995
Barham Slah <barhamsalahaddin@gmail.com>
2026-08-04 18:57:17 +0300

    Add code to Write-Files.java

--- Write-Files.java
+++ Write-Files.java
@@ -6,3 +6,26 @@ BufferedWriter :  For large amounts of text and big files
 PrintWriter :  For creating structured logs and reports.
 FileOutputStream :  For binary files (images, audio, video).
 
+
+
+***************************************************************************************************************
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String store = "This code absolutely was written by me";
+
+        try(FileWriter fileWriter = new FileWriter("MyFile.txt")) {
+            fileWriter.write(store);
+        } catch (FileNotFoundException e){
+            System.out.println("Your file path is wrong");
+        } catch (IOException e){
+            System.out.println("Your issue is " + e.getMessage());
+        }
+
+    }
+}
